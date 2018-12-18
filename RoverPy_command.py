bfe0c3cd0b7fc41325c297e0ffd67cbdb188974d
PiKSo05 <vilain.vincent@gmail.com>
2018-12-18 22:49:23 +0100

    Update RoverPy_command.py
    correction de l'indentation

--- RoverPy_command.py
+++ RoverPy_command.py
@@ -3,83 +3,84 @@ import time
 import sys
 
 class RoverPyCommand:
-
 	def init():
 		GPIO.setmode(GPIO.BCM)
-	    	#GPIO.setmode(gpio.BOARD)
+		#GPIO.setmode(gpio.BOARD)
 		GPIO.setup(22, GPIO.OUT)
-    		GPIO.setup(23, GPIO.OUT)
-	    	GPIO.setup(24, GPIO.OUT)
-    		GPIO.setup(25, GPIO.OUT)
-
+		GPIO.setup(23, GPIO.OUT)
+		GPIO.setup(24, GPIO.OUT)
+		GPIO.setup(25, GPIO.OUT)
+		
 	def forward(tf):
-		init()GPIO.output(22, GPIO.HIGH)
-    		GPIO.output(23, GPIO.LOW)
-    		GPIO.output(25, GPIO.HIGH)
-    		GPIO.output(24, GPIO.LOW)
-    		time.sleep(tf)
-    		GPIO.cleanup()
+		init()
+		GPIO.output(22, GPIO.HIGH)
+		GPIO.output(23, GPIO.LOW)
+		GPIO.output(25, GPIO.HIGH)
+		GPIO.output(24, GPIO.LOW)
+		time.sleep(tf)
+		GPIO.cleanup()
 
 	def backward(tf):
-    		init()
-    		GPIO.output(22, GPIO.LOW)
-    		GPIO.output(23, GPIO.HIGH)
-    		GPIO.output(25, GPIO.LOW)
-    		GPIO.output(24, GPIO.HIGH)
-    		time.sleep(tf)
-    		GPIO.cleanup()
+		init()
+		GPIO.output(22, GPIO.LOW)
+		GPIO.output(23, GPIO.HIGH)
+		GPIO.output(25, GPIO.LOW)
+		GPIO.output(24, GPIO.HIGH)
+		time.sleep(tf)
+		GPIO.cleanup()
 
 	def turnRight(tf):
-    		init()
-    		GPIO.output(22, GPIO.HIGH)
+		init()
+		GPIO.output(22, GPIO.HIGH)
 		GPIO.output(23, GPIO.LOW)
-    		GPIO.output(25, GPIO.LOW)
-    		GPIO.output(24, GPIO.LOW)
-    		time.sleep(tf)
-    		GPIO.cleanup()
+		GPIO.output(25, GPIO.LOW)
+		GPIO.output(24, GPIO.LOW)
+		time.sleep(tf)
+		GPIO.cleanup()
+		
 	def turnLeft(tf):
 		init()
 		GPIO.output(22, GPIO.LOW)
-    		GPIO.output(23, GPIO.LOW)
-    		GPIO.output(25, GPIO.HIGH)
-    		GPIO.output(24, GPIO.LOW)
-    		time.sleep(tf)
-    		GPIO.cleanup()
+		GPIO.output(23, GPIO.LOW)
+		GPIO.output(25, GPIO.HIGH)
+		GPIO.output(24, GPIO.LOW)
+		time.sleep(tf)
+		GPIO.cleanup()
 
 	def pivotRight(tf):
-    		init()
-    		GPIO.output(22, GPIO.HIGH)
-    		GPIO.output(23, GPIO.LOW)
-    		GPIO.output(25, GPIO.LOW)
-    		GPIO.output(24, GPIO.HIGH)
-    		time.sleep(tf)
-    		GPIO.cleanup()
+		init()
+		GPIO.output(22, GPIO.HIGH)
+		GPIO.output(23, GPIO.LOW)
+		GPIO.output(25, GPIO.LOW)
+		GPIO.output(24, GPIO.HIGH)
+		time.sleep(tf)
+		GPIO.cleanup()
 
 	def pivotLeft(tf):
-    		init()
-    		GPIO.output(22, GPIO.LOW)
-    		GPIO.output(23, GPIO.HIGH)
-    		GPIO.output(25, GPIO.HIGH)
-    		GPIO.output(24, GPIO.LOW)
-    		time.sleep(tf)
-    		GPIO.cleanup()
+		init()
+		GPIO.output(22, GPIO.LOW)
+		GPIO.output(23, GPIO.HIGH)
+		GPIO.output(25, GPIO.HIGH)
+		GPIO.output(24, GPIO.LOW)
+		time.sleep(tf)
+		GPIO.cleanup()
 
 	def keyInput(event):
-    		init()
-    		print 'key: ', event.char
-    		keyPress = event.char
-    		sleepTime = 0.030
+		init()
+		print 'key: ', event.char
+		keyPress = event.char
+		sleepTime = 0.030
 		if keyPress.lower() == 'z':
-        		forward(sleepTime)
-    		elif keyPress.lower() == 's':
-        		backward(sleepTime)
-    		elif keyPress.lower() == 'q':
-        		turnLeft(sleepTime)
-    		elif keyPress.lower() == 'd':
-        		turnRight(sleepTime)
-    		elif keyPress.lower() == 'w':
-        		pivotLeft(sleepTime)
-    		elif keyPress.lower() == 'c':
-        		pivotRight(sleepTime)
-    		else:
-        		pass
+			forward(sleepTime)
+		elif keyPress.lower() == 's':
+			backward(sleepTime)
+		elif keyPress.lower() == 'q':
+			turnLeft(sleepTime)
+		elif keyPress.lower() == 'd':
+			turnRight(sleepTime)
+		elif keyPress.lower() == 'w':
+			pivotLeft(sleepTime)
+		elif keyPress.lower() == 'c':
+			pivotRight(sleepTime)
+		else:
+			pass
