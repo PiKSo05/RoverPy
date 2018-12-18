defb47845a47e73c82a479159da72e2156306b28
PiKSo05 <vilain.vincent@gmail.com>
2018-12-18 22:39:53 +0100

    ajout d'une classe de commande, d'un fichier de test et d'une ébauche de service

---
+++ RoverPy_test.py
@@ -0,0 +1,23 @@
+from RoverPy_command.py import RoverPyCommand
+
+roverPyCommand = RoverPyCommand()
+
+roverPyCommand.forward(1)
+#backward(1)
+#turnLeft(0.05)
+#turnRight(1)
+#pivotLeft(1)
+#pivotRight(0.1)
+
+##GPIO.setmode(GPIO.BCM)
+##GPIO.setup(22, GPIO.OUT)
+##GPIO.setup(23, GPIO.OUT)
+##GPIO.setup(24, GPIO.OUT)
+##GPIO.setup(25, GPIO.OUT)
+##     
+##GPIO.output(22, GPIO.HIGH)
+####GPIO.output(23, GPIO.HIGH)
+##GPIO.output(25, GPIO.HIGH)
+####GPIO.output(24, GPIO.LOW)
+##time.sleep(1)
+##GPIO.cleanup()
