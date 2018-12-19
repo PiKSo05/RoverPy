5795311ea08dcf4943d999b31e068079b8c0da8d
PiKSo05 <vilain.vincent@gmail.com>
2018-12-19 13:26:59 +0100

    Update RoverPy_KeyBoardControl.py

--- RoverPy_KeyBoardControl.py
+++ RoverPy_KeyBoardControl.py
@@ -15,21 +15,21 @@ class KeyBoardControl:
       print 'constructeur'
     
   def keyInput(self, event):
-		print 'key: ', event.name
-		keyPress = event.name
-		if keyPress.lower() == self.forwardKey:
-			self.forward(self.sleepTime)
-		elif keyPress.lower() == self.backwardKey:
-			self.backward(self.sleepTime)
-		elif keyPress.lower() == self.turnLeftKey:
-			self.turnLeft(self.sleepTime)
-		elif keyPress.lower() == self.turnRightKey:
-			self.turnRight(self.sleepTime)
-		elif keyPress.lower() == self.pivotLeftKey:
-			self.pivotLeft(self.sleepTime)
-		elif keyPress.lower() == self.pivotRightKey:
-			self.pivotRight(self.sleepTime)
-		elif keyPress.lower() == self.stopKey:
-			self.stop()
-		else:
-			pass
+	print 'key: ', event.name
+	keyPress = event.name
+	if keyPress.lower() == self.forwardKey:
+		self.forward(self.sleepTime)
+	elif keyPress.lower() == self.backwardKey:
+		self.backward(self.sleepTime)
+	elif keyPress.lower() == self.turnLeftKey:
+		self.turnLeft(self.sleepTime)
+	elif keyPress.lower() == self.turnRightKey:
+		self.turnRight(self.sleepTime)
+	elif keyPress.lower() == self.pivotLeftKey:
+		self.pivotLeft(self.sleepTime)
+	elif keyPress.lower() == self.pivotRightKey:
+		self.pivotRight(self.sleepTime)
+	elif keyPress.lower() == self.stopKey:
+		self.stop()
+	else:
+		pass
